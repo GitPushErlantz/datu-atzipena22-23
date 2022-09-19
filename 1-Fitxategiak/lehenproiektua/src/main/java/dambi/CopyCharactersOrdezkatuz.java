f0eb37a9706e8d683efc919da8329b088884fd7d
Erlantz <garate.erlantz@uni.eus>
2022-09-19 12:23:33 +0200

    lehen proiektua

---
+++ 1-Fitxategiak/lehenproiektua/src/main/java/dambi/CopyCharactersOrdezkatuz.java
@@ -0,0 +1,36 @@
+package dambi;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyCharactersOrdezkatuz {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("./xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                if (c == 97) {
+                    outputStream.write(111);
+                } else if (c == 65) {
+                    outputStream.write(79);
+                } else {
+                    outputStream.write(c);
+                }
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
