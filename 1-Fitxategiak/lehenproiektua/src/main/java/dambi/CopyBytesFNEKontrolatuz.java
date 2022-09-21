8c8002ef95e27cf5e50bc96dffdbdc32375dd4ff
Erlantz <garate.erlantz@uni.eus>
2022-09-21 10:26:41 +0200

    ariketa gehiago

---
+++ 1-Fitxategiak/lehenproiektua/src/main/java/dambi/CopyBytesFNEKontrolatuz.java
@@ -0,0 +1,33 @@
+package dambi;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytesFNEKontrolatuz {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("./xanadu.txt");
+            out = new FileOutputStream("outagain.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("File not found! Please try again");
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
---
+++ 1-Fitxategiak/lehenproiektua/src/main/java/dambi/CopyLinesZenbakiekin.java
@@ -0,0 +1,34 @@
+package dambi;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class CopyLinesZenbakiekin {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        try {
+            inputStream = new BufferedReader(new FileReader("xanadu.txt"));
+            outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
+
+            int lineNum = 1;
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                outputStream.println(lineNum + " " + l);
+                ++lineNum;
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
